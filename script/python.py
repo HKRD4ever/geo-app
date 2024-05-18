51c2399349855a974209301a1583ec7c2f172889
Kenslor Ralph Donald HYACINTHE <hyacinthe.store@gmail.com>
2024-05-18 17:22:34 -0400

    pythoncode

--- script/python.py
+++ script/python.py
@@ -8,6 +8,6 @@ os.system("uname -r")  # This will work on Unix-like systems (Linux, macOS)
 =======
 print("hello kenslor)
 
-os.system("date")
+os.system("nproc")
 os.system("uname-r")
 >>>>>>> 64858a9dfaae3fee63052f4a1219dc534d1213d5
